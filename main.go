c46775788658ce78c8b02547e41716fefd9e2179
Manuel Porto <manu.porto94@gmail.com>
2019-07-09 09:08:58 +0200

    Host selection policies example (#1)
    * Simple script of GoCQL tweaked to show the different possible policies
    
    * Add TokenAware host policy
    
    * Use a real table in the example

---
+++ main.go
@@ -0,0 +1,60 @@
+/* Before you execute the program, Launch `cqlsh` and execute:
+CREATE KEYSPACE hydro_monitor_data WITH REPLICATION = { 'class' : 'SimpleStrategy', 'replication_factor' : 3 };
+CREATE TABLE hydro_monitor_data.users (email string, password string, admin bool, primary key (email));
+*/
+package main
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/gocql/gocql"
+	// "github.com/hailocab/go-hostpool"
+)
+
+func main() {
+	// connect to the cluster
+	cluster := gocql.NewCluster("192.168.50.41", "192.168.50.42", "192.168.50.43")
+
+	// cluster.PoolConfig.HostSelectionPolicy = gocql.DCAwareRoundRobinPolicy("local-datacenter-name")
+
+	// Create host selection policy using a simple host pool
+	// cluster.PoolConfig.HostSelectionPolicy = gocql.HostPoolHostPolicy(hostpool.New(nil))
+	// Create host selection policy using an epsilon greedy pool
+	// cluster.PoolConfig.HostSelectionPolicy = gocql.HostPoolHostPolicy(
+		// hostpool.NewEpsilonGreedy(nil, 0, &hostpool.LinearEpsilonValueCalculator{}),)
+	
+	// cluster.PoolConfig.HostSelectionPolicy = gocql.RoundRobinHostPolicy()
+	cluster.PoolConfig.HostSelectionPolicy = gocql.TokenAwareHostPolicy(gocql.RoundRobinHostPolicy())
+	cluster.Keyspace = "example"
+	cluster.Consistency = gocql.Quorum
+	session, _ := cluster.CreateSession()
+	defer session.Close()
+
+	// insert a user
+	if err := session.Query(`INSERT INTO users (email, password, admin) VALUES (?, ?, ?)`,
+		"bob@example.com", "secretandencryptedpassword", false).Exec(); err != nil {
+		log.Fatal(err)
+	}
+
+	var email string
+
+	/* Search for a specific set of records whose 'timeline' column matches
+	 * the value 'me'. The secondary index that we created earlier will be
+	 * used for optimizing the search */
+	// if err := session.Query(`SELECT id, text FROM tweet WHERE timeline = ? LIMIT 1`,
+	if err := session.Query(`SELECT email FROM users WHERE admin = ? LIMIT 1`,
+		true).Consistency(gocql.One).Scan(&email); err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("Admin:", email)
+
+	// list all users
+	iter := session.Query(`SELECT email FROM users`).Iter()
+	for iter.Scan(&email) {
+		fmt.Println("User:", email)
+	}
+	if err := iter.Close(); err != nil {
+		log.Fatal(err)
+	}
+}
